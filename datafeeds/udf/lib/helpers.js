8d162cc605f6d0add1c31d030740f16487395fb2
1kxexchange <deploy_bot@x.com>
2022-11-04 09:19:05 +0000

    See https://github.com/1kxexchange/1kx-sourceCode/commit/08cfad1e6e805e5da4a42e7e4f6846049c46f303 from refs/heads/main

--- datafeeds/udf/lib/helpers.js
+++ datafeeds/udf/lib/helpers.js
@@ -1,1 +1,20 @@
-const isLoggingEnabled=!1;export function logMessage(e){if(isLoggingEnabled){const o=new Date;console.log(`${o.toLocaleTimeString()}.${o.getMilliseconds()}> ${e}`)}}export function getErrorMessage(e){return void 0===e?"":"string"===typeof e?e:e.message}
+/**
+ * If you want to enable logs from datafeed set it to `true`
+ */
+const isLoggingEnabled = false;
+export function logMessage(message) {
+    if (isLoggingEnabled) {
+        const now = new Date();
+        // tslint:disable-next-line:no-console
+        console.log(`${now.toLocaleTimeString()}.${now.getMilliseconds()}> ${message}`);
+    }
+}
+export function getErrorMessage(error) {
+    if (error === undefined) {
+        return '';
+    }
+    else if (typeof error === 'string') {
+        return error;
+    }
+    return error.message;
+}
--- datafeeds/udf/lib/udf-compatible-datafeed.js
+++ datafeeds/udf/lib/udf-compatible-datafeed.js
@@ -1,1 +1,10 @@
-import{UDFCompatibleDatafeedBase}from"./udf-compatible-datafeed-base";import{QuotesProvider}from"./quotes-provider";import{Requester}from"./requester";export class UDFCompatibleDatafeed extends UDFCompatibleDatafeedBase{constructor(e,t=1e4){const o=new Requester,r=new QuotesProvider(e,o);super(e,r,o,t)}}
+import { UDFCompatibleDatafeedBase } from './udf-compatible-datafeed-base';
+import { QuotesProvider } from './quotes-provider';
+import { Requester } from './requester';
+export class UDFCompatibleDatafeed extends UDFCompatibleDatafeedBase {
+    constructor(datafeedURL, updateFrequency = 10 * 1000) {
+        const requester = new Requester();
+        const quotesProvider = new QuotesProvider(datafeedURL, requester);
+        super(datafeedURL, quotesProvider, requester, updateFrequency);
+    }
+}
